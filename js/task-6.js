7e9731951e000de3092bac9fdcb54bf16bcd43bf
Mariana7IT <153327568+Mariana7IT@users.noreply.github.com>
2024-05-03 01:45:06 +0300

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -46,3 +46,44 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
+// приклад алгоритму дій:
+// const input = document.querySelector("input");
+// const create = document.querySelector("button[data-create]");
+// const destroy = document.querySelector("button[data-destroy]");
+// const box = document.querySelector("#boxes");
+
+// let sizes = 30;
+
+// create.addEventListener("click", createMarkup);
+// destroy.addEventListener("click", () => {
+//   box.innerHTML = "";
+//   sizes = 30;
+// });
+
+
+// function createMarkup() {
+//   if (input.value < 1 || input.value > 100) {
+//     console.log("error");
+//     return;
+//   }
+
+//   createBoxes(input.value);
+// }
+
+// function createBoxes(amount) {
+//   input.value = "";
+//   box.innerHTML = "";
+//   sizes = 30;
+
+  
+//   for (let i = 0; i < amount, i++){
+//     const myBox = document.createElement("div");
+//     myBox.style.width = `${sizes}px`;
+//     myBox.style.height = `${height}px`;
+//     myBox.style.backgroundColor = getRandomHexColor();
+//     box.append(myBox);
+
+//     sizes += 10;
+//   }
+  
+// }
